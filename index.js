4c1cbbc939ab4a6565ec342334f21bd71add215b
PETER ONYAM <peteronyam@gmail.com>
2024-09-03 16:25:27 +0100

    filejs

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-console.log(`HELLO`);
-console.log('I like coding');
+// console.log(`HELLO`);
+// console.log('I like coding');
 
-window.alert(`This is an alert`);
+// // window.alert(`This is an alert`);
 
-//this is a comment
+// //this is a comment
 
-document.getElementById("HEADER1").textContent = `my dome file`;
-document.getElementById("myip").textContent = `this is the second paragraph of the dom file`;
-/* this is a long paragraph comment
-we should not see this in the console */
+// document.getElementById("HEADER1").textContent = `my dome file`;
+// document.getElementById("myip").textContent = `this is the second paragraph of the dom file`;
+// /* this is a long paragraph comment
+// we should not see this in the console */
+// let x;
+// let y;
+// x = 100;
+// y = "pips";
+// console.log(x, y);
+// let age = 25;
+// let price = 999;
+// let gpa = 2.1;
+// let time = "tip";
+// console.log(`you are ${age} years old`);
+// console.log(`the price is: $${price}`);
+// console.log(`your gpa is: $(gpa)`);
+// console.log(typeof(age));
+
+// let students = 30;
+// // students = students + 3;
+// students **= 5;
+// students %= 5;
+// console.log(students);
+
+// username =window.prompt("what is your Username?");
+// console.log(username);
+
+
+// document.getElementById("submit").onclick = function(){
+//     username = document.getElementById("user").value;
+//     console.log(username);
+// }
+let username;
+
+document.getElementById("submit").onclick = function(){
+    username = document.getElementById("user").value;
+    document.getElementById("head1").textContent = `Hello ${username}`;
+}
