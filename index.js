2fa4e226eaf7e21598db40489072373879d955b9
PETER ONYAM <peteronyam@gmail.com>
2024-09-03 14:48:11 +0100

    files

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log(`HELLO`);
+console.log('I like coding');
+
+window.alert(`This is an alert`);
+
+//this is a comment
+
+document.getElementById("HEADER1").textContent = `my dome file`;
+document.getElementById("myip").textContent = `this is the second paragraph of the dom file`;
+/* this is a long paragraph comment
+we should not see this in the console */
