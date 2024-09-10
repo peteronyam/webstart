9e0fc24c48ba9cce39d08e0f178034e51d6bf774
PETER ONYAM <peteronyam@gmail.com>
2024-09-11 00:26:39 +0100

    add file

--- index.js
+++ index.js
@@ -187,9 +187,16 @@ const character = "#";
 const count = 8;
 const rows = [];
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(i);
+    rows.push(character.repeat(i +1));
 };
 let result = "";
  for(const row of rows){
-
+    result = result + "\n" + row;
  };
+ console.log(result);
+
+ function padRow(name) {
+    return "Hello!";
+  };
+  const call = padRow();
+  console.log(call);
