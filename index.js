9f08c645e5696db0e184e1ff5b8e9553e2557e55
PETER ONYAM <peteronyam@gmail.com>
2024-10-07 15:13:23 +0100

    add while in the middle

--- index.js
+++ index.js
@@ -425,6 +425,13 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}
 
 for (let i = count; i > 0; i = i - 1) {
   rows.push(padRow(i, count))
