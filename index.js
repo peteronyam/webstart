027f0293d7c5e68fe8427a68bae22a92837ab840
PETER ONYAM <peteronyam@gmail.com>
2024-10-14 17:43:47 +0100

    add gradescore

--- index.js
+++ index.js
@@ -522,3 +522,50 @@ function getAverage(scores) {
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+// // Step 2
+// Now the teacher needs your help converting the student score to a letter grade.
+
+// Complete the getGrade function that takes a number score as a parameter. Your function should return a string representing a letter grade based on the score.
+
+// Here are the scores and their corresponding letter grades:
+
+// Score Range	Grade
+// 100	"A++"
+// 90 - 99	"A"
+// 80 - 89	"B"
+// 70 - 79	"C"
+// 60 - 69	"D"
+// 0 - 59	"F"
+// Tips
+
+// Remember that you learned about conditional statements (if, else if, and else).
+// Remember that you learned about comparison operators (>, <, >=, <=, ===).
+
+
+function getGrade(score) {
+  if(score == 100){
+    return "A++";
+  }else if(score >= 90 && score <= 99){
+    return "A"
+  }else if(score <= 89 && score >= 80){
+    return "B"
+  }else if(score >= 70 && score <= 79){
+    return "C"
+  }else if(score <= 69 && score >= 60){
+    return "D"
+  }else if(score >= 0 && score <= 59){
+    return "F"
+  }else{
+    console.log("scores not found")
+  }
+
+}
+
+console.log(getGrade(100));
+console.log(getGrade(92));
+console.log(getGrade(82));
+console.log(getGrade(73));
+console.log(getGrade(67));
+console.log(getGrade(56));
